c463be23ebd88131e64a264f9796a9df35eba488
MagicalBits <MagicalBits@outlook.com>
2014-05-28 13:47:41 +0100

    Sources

---
+++ DSEDataExtractor/Form1.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DSEDE
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.cboMSTSource = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.cboDestinationFolder = New System.Windows.Forms.ComboBox()
+        Me.cboIndexSymbolName = New System.Windows.Forms.ComboBox()
+        Me.txtStatus = New System.Windows.Forms.TextBox()
+        Me.chkValidationCheck = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(66, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(117, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "DSE MST File Source: "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(66, 68)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(107, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "DSEX Symbol Name:"
+        '
+        'cboMSTSource
+        '
+        Me.cboMSTSource.FormattingEnabled = True
+        Me.cboMSTSource.Items.AddRange(New Object() {"http://www.dsebd.org/mst.txt"})
+        Me.cboMSTSource.Location = New System.Drawing.Point(184, 38)
+        Me.cboMSTSource.Name = "cboMSTSource"
+        Me.cboMSTSource.Size = New System.Drawing.Size(323, 21)
+        Me.cboMSTSource.TabIndex = 8
+        Me.cboMSTSource.Text = "http://www.dsebd.org/mst.txt"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(66, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 13)
+        Me.Label2.TabIndex = 10
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(66, 94)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(92, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Destination Folder"
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(69, 243)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(436, 57)
+        Me.btnGo.TabIndex = 12
+        Me.btnGo.Text = "Go!"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'cboDestinationFolder
+        '
+        Me.cboDestinationFolder.FormattingEnabled = True
+        Me.cboDestinationFolder.Location = New System.Drawing.Point(184, 91)
+        Me.cboDestinationFolder.Name = "cboDestinationFolder"
+        Me.cboDestinationFolder.Size = New System.Drawing.Size(323, 21)
+        Me.cboDestinationFolder.TabIndex = 15
+        '
+        'cboIndexSymbolName
+        '
+        Me.cboIndexSymbolName.FormattingEnabled = True
+        Me.cboIndexSymbolName.Items.AddRange(New Object() {"00DSEGEN", "DSEX"})
+        Me.cboIndexSymbolName.Location = New System.Drawing.Point(184, 64)
+        Me.cboIndexSymbolName.Name = "cboIndexSymbolName"
+        Me.cboIndexSymbolName.Size = New System.Drawing.Size(323, 21)
+        Me.cboIndexSymbolName.TabIndex = 16
+        Me.cboIndexSymbolName.Text = "00DSEGEN"
+        '
+        'txtStatus
+        '
+        Me.txtStatus.BackColor = System.Drawing.Color.LightGray
+        Me.txtStatus.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtStatus.Location = New System.Drawing.Point(69, 119)
+        Me.txtStatus.Multiline = True
+        Me.txtStatus.Name = "txtStatus"
+        Me.txtStatus.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtStatus.Size = New System.Drawing.Size(437, 64)
+        Me.txtStatus.TabIndex = 17
+        Me.txtStatus.Text = "Click Go! to get data for today"
+        Me.txtStatus.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'chkValidationCheck
+        '
+        Me.chkValidationCheck.AutoSize = True
+        Me.chkValidationCheck.Location = New System.Drawing.Point(196, 199)
+        Me.chkValidationCheck.Name = "chkValidationCheck"
+        Me.chkValidationCheck.Size = New System.Drawing.Size(169, 17)
+        Me.chkValidationCheck.TabIndex = 18
+        Me.chkValidationCheck.Text = "Perform extra validation check"
+        Me.chkValidationCheck.UseVisualStyleBackColor = True
+        '
+        'DSEDE
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(561, 312)
+        Me.Controls.Add(Me.chkValidationCheck)
+        Me.Controls.Add(Me.txtStatus)
+        Me.Controls.Add(Me.cboIndexSymbolName)
+        Me.Controls.Add(Me.cboDestinationFolder)
+        Me.Controls.Add(Me.btnGo)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.cboMSTSource)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "DSEDE"
+        Me.Text = "DSE Data Extractor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents cboMSTSource As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents btnGo As System.Windows.Forms.Button
+    Friend WithEvents cboDestinationFolder As System.Windows.Forms.ComboBox
+    Friend WithEvents cboIndexSymbolName As System.Windows.Forms.ComboBox
+    Friend WithEvents txtStatus As System.Windows.Forms.TextBox
+    Friend WithEvents chkValidationCheck As System.Windows.Forms.CheckBox
+
+End Class
---
+++ DSEDataExtractor/Form1.vb
@@ -0,0 +1,261 @@
+﻿Imports System.Net
+Imports System.IO
+
+Public Class DSEDE
+
+    Dim MSTLines() As String
+    Dim StockLines() As String
+    Dim StockInfo(,) As String
+    Dim DSEXInfo(4) As String
+    Dim DS30Info(4) As String
+    Dim MSTDate, DSEXDate, DS30Date As Date
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles btnGo.Click
+
+        SetStatus("Retrieving MST Data...")
+        Me.Cursor = Cursors.AppStarting
+
+        Try
+            If Not RetrieveMSTData() Then
+                SetStatus("Could not retrieve MST File Data, check if MST file address is correct and you have internet connection")
+                Me.Cursor = Cursors.Default
+                Exit Sub
+            End If
+
+            SetStatus("Processing MST File...")
+
+            GetStockLines()
+
+            ProcessStockLines()
+
+            SetStatus("Getting Index Information...")
+
+            GetIndexHighLow()
+
+            If Not DoesMSTIndexDateMatch() Then
+                SetStatus("DSEX/DS30 and MST Date not matching. Please try later when the MST file is updated on DSE's Website.")
+                Me.Cursor = Cursors.Default
+                Exit Sub
+            End If
+
+            SetStatus("Writing CSV file...")
+
+            If chkValidationCheck.Checked Then
+                If Not ValidationCheckBeforeWritingCSV() Then
+                    SetStatus("An Error occurred while retrieving stock and index info. Perhaps the format of DSE's MST file was changed. Contact the Developer.")
+                    Me.Cursor = Cursors.Default
+                    Exit Sub
+                End If
+            End If
+
+            WriteCSV()
+
+        Catch ex As Exception
+            SetStatus(ex.ToString)
+        End Try
+
+        Me.Cursor = Cursors.Default
+        SetStatus("Done! CSV file saved to the specified location.")
+
+    End Sub
+
+    Private Function ValidationCheckBeforeWritingCSV() As Boolean
+        Try
+            Dim i As Short
+
+            For i = 0 To 4
+                Dim TempResult As Single
+                If Not Single.TryParse(DSEXInfo(i), TempResult) Then
+                    Return False
+                End If
+
+                If Not Single.TryParse(DS30Info(i), TempResult) Then
+                    Return False
+                End If
+            Next
+
+            For i = 0 To StockInfo.GetUpperBound(0) - 1
+                If StockInfo(i, 0) <> "" Then
+                    Dim j As Short
+                    For j = 1 To 5
+                        Dim TempResult As Single
+
+                        If Not Single.TryParse(StockInfo(i, j), TempResult) Then
+                            Return False
+                        End If
+                    Next
+                End If
+            Next
+
+            If DSEXDate.Equals(DateTime.MinValue) Or DS30Date.Equals(DateTime.MinValue) Or MSTDate.Equals(DateTime.MinValue) Then
+                Return False
+            End If
+
+            Return True
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+
+    Private Sub WriteCSV()
+        Dim CSVName As String = "DSE_" & Format(MSTDate, "ddMMyy") & ".csv"
+        Dim FinalCSVPath As String = cboDestinationFolder.Text & "\" & CSVName
+        Dim CSVString = BuildCSVString()
+
+        SaveTextToFile(CSVString, FinalCSVPath)
+    End Sub
+
+    Private Function BuildCSVString() As String
+        Dim DSEXLine, DS30Line As String
+        DSEXLine = cboIndexSymbolName.Text & "," & Format(MSTDate, "yyyy-MM-dd") & "," & _
+            DSEXInfo(0) & "," & DSEXInfo(1) & "," & DSEXInfo(2) & "," & DSEXInfo(3) & "," & DSEXInfo(4)
+        DS30Line = "DS30" & "," & Format(MSTDate, "yyyy-MM-dd") & "," & _
+            DS30Info(0) & "," & DS30Info(1) & "," & DS30Info(2) & "," & DS30Info(3) & "," & DS30Info(4)
+
+        BuildCSVString = DSEXLine & vbCrLf & DS30Line & vbCrLf
+
+        Dim StockCounter As Short
+
+        For StockCounter = 0 To StockInfo.GetUpperBound(0) - 1
+            If StockInfo(StockCounter, 0) <> "" Then
+                BuildCSVString += StockInfo(StockCounter, 0) & "," & Format(MSTDate, "yyyy-MM-dd") & _
+                    "," & StockInfo(StockCounter, 1) & "," & StockInfo(StockCounter, 2) & _
+                    "," & StockInfo(StockCounter, 3) & "," & StockInfo(StockCounter, 4) & _
+                    "," & StockInfo(StockCounter, 5) & vbCrLf
+            End If
+        Next
+    End Function
+
+    Private Function DoesMSTIndexDateMatch() As Boolean
+        If DSEXDate.Equals(MSTDate) And DS30Date.Equals(MSTDate) Then
+            Return True
+        End If
+
+        Return False
+    End Function
+
+    Private Sub GetIndexHighLow()
+        Dim DSEXData As String = GetPage("http://dsebd.org/index-graph/companygraph.php?graph_id=dsbi")
+        Dim DS30Data As String = GetPage("http://dsebd.org/index-graph/companygraph.php?graph_id=ds30")
+
+        DSEXInfo(1) = Trim(Strings.Mid(DSEXData, DSEXData.IndexOf("Highest value:</strong> ") + 24, 10))
+        DSEXInfo(2) = Trim(Strings.Mid(DSEXData, DSEXData.IndexOf("Lowest value:</strong> ") + 23, 10))
+        DS30Info(1) = Trim(Strings.Mid(DS30Data, DS30Data.IndexOf("Highest value:</strong> ") + 24, 10))
+        DS30Info(2) = Trim(Strings.Mid(DS30Data, DS30Data.IndexOf("Lowest value:</strong> ") + 23, 10))
+
+        DSEXDate = Strings.Mid(DSEXData, DSEXData.IndexOf(" of ") + 4, 11)
+        DS30Date = Strings.Mid(DS30Data, DS30Data.IndexOf(" of ") + 4, 11)
+    End Sub
+
+    Public Function SaveTextToFile(ByVal strData As String, _
+     ByVal FullPath As String, _
+       Optional ByVal ErrInfo As String = "") As Boolean
+
+        Dim bAns As Boolean = False
+        Dim objReader As StreamWriter
+        Try
+            objReader = New StreamWriter(FullPath)
+            objReader.Write(strData)
+            objReader.Close()
+            bAns = True
+        Catch Ex As Exception
+            ErrInfo = Ex.Message
+
+        End Try
+        Return bAns
+    End Function
+
+    Private Function GetDesktopDirectory() As String
+        Return Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+    End Function
+
+    Private Sub ProcessStockLines()
+        Dim i As Short
+        ReDim StockInfo(StockLines.Length, 5)
+
+        For i = 0 To StockLines.Length - 1
+            Dim TempStockLine As String = StockLines(i)
+
+            StockInfo(i, 0) = Trim(Strings.Mid(TempStockLine, 2, 11))
+            StockInfo(i, 1) = Trim(Strings.Mid(TempStockLine, 12, 9))
+            StockInfo(i, 2) = Trim(Strings.Mid(TempStockLine, 21, 9))
+            StockInfo(i, 3) = Trim(Strings.Mid(TempStockLine, 30, 9))
+            StockInfo(i, 4) = Trim(Strings.Mid(TempStockLine, 39, 9))
+            StockInfo(i, 5) = Trim(Strings.Mid(TempStockLine, 62, 10))
+        Next
+    End Sub
+
+    Private Function IsThisStockLine(Line As String) As Boolean
+        If Line <> "" Then
+            If ((Line.Length = 81) Or (Line.Length = 80)) And (Not String.IsNullOrWhiteSpace(Strings.Left(Line, 2))) And (IsNumeric(Strings.Right(Line, 2))) Then
+                Return True
+            End If
+        End If
+        Return False
+    End Function
+
+    Private Sub GetStockLines()
+        Dim i, SubCounter As Short
+        For i = 0 To MSTLines.Length - 1
+            If IsThisStockLine(MSTLines(i)) Then
+                ReDim Preserve StockLines(SubCounter)
+                StockLines(SubCounter) = MSTLines(i)
+                SubCounter += 1
+            End If
+
+            If (MSTLines(i).StartsWith(vbLf & "DS30")) Then
+                DS30Info(0) = Trim(Strings.Mid(MSTLines(i), 12, 15))
+                DS30Info(3) = Trim(Strings.Mid(MSTLines(i), 27))
+            End If
+
+            If (MSTLines(i).StartsWith(vbLf & "DSEX")) Then
+                DSEXInfo(0) = Trim(Strings.Mid(MSTLines(i), 12, 15))
+                DSEXInfo(3) = Trim(Strings.Mid(MSTLines(i), 27))
+            End If
+
+            If (MSTLines(i).StartsWith(vbLf & "    C. VALUE(Tk)")) Then
+                Dim IndexTradeValue As String = Trim(Strings.Mid(MSTLines(i), 40) / 100)
+                DSEXInfo(4) = IndexTradeValue
+                DS30Info(4) = IndexTradeValue
+            End If
+
+            If (MSTLines(i).StartsWith(vbLf & "                  TODAY'S SHARE MARKET")) Then
+                MSTDate = Date.Parse(Strings.Right(MSTLines(i), 10))
+            End If
+        Next
+    End Sub
+
+    Private Sub SetStatus(ByVal Message As String)
+        txtStatus.Text = Message
+    End Sub
+
+    Private Function RetrieveMSTData() As Boolean
+        Dim MSTData As String = GetPage(cboMSTSource.Text)
+        If MSTData = "" Then
+            Return False
+        End If
+
+        MSTLines = MSTData.Split(Chr(13))
+        Return True
+    End Function
+
+    Private Function GetPage(ByVal PageURL As String) As String
+        Dim S As String = ""
+        Try
+            Dim Request As HttpWebRequest = WebRequest.Create(PageURL)
+            Dim Response As HttpWebResponse = Request.GetResponse()
+            Using Reader As StreamReader = New StreamReader(Response.GetResponseStream())
+                S = Reader.ReadToEnd
+            End Using
+        Catch ex As Exception
+            S = ""
+        End Try
+        Return S
+    End Function
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Dim DesktopDir As String = GetDesktopDirectory()
+        cboDestinationFolder.Items.Add(DesktopDir)
+        cboDestinationFolder.Text = DesktopDir
+    End Sub
+End Class
